5f76022229bb44b80bc0db6f7016e857c7fbe456
mukeshkanna11 <agriculturistmukesh11@gmail.com>
2024-09-14 12:25:00 +0530

    First Commit

---
+++ Tailwindcli/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily:{
+
+        righteous:["righteous","sans-serif"],
+        rowdies:["Rowdies", "sans-serif"],
+        saira:["saira", "sans-serf "]
+       }
+    },
+  },
+  plugins: [],
+}
+
